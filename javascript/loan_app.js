931805addad7654611598fa6ab5ea840fc752236
Edward Cable <edcable@gmail.com>
2014-07-24 19:37:19 -0700

    first version

---
+++ javascript/loan_app.js
@@ -0,0 +1,30 @@
+{
+   "basic_details": {
+      "product_id": "BGML",
+      "product_name": "BGS Monthly Loan",
+      "currency": "USD",
+      "loan_purpose": "Animal Husbandry"
+   },
+   "loan_terms": {
+      "interest_rate": ".05",
+      "interest_type": "flat",
+      "number_installments": "12",
+      "principal_amount": "5000"
+   },
+   "charges": [
+      {
+         "id": "001",
+         "shortname": "MLC",
+         "name": "Monthly Loan Charge",
+         "type": "Periodic",
+         "amount": "5"
+      },
+      {
+         "id": "002",
+         "shortname": "OLC",
+         "name": "One-Time Loan Charge",
+         "type": "one-time",
+         "amount": "500"
+      }
+   ]
+}
